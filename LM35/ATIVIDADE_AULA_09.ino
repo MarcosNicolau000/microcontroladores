e61d89a7dfb50556cea440069defd2aac6c43df7
MarcosNicolau000 <marcosnicolauoliveira@outlook.com>
2025-10-21 08:21:24 -0300

    Exercicios - LCD-LM35

---
+++ LM35/ATIVIDADE_AULA_09.ino
@@ -0,0 +1,29 @@
+#include <LiquidCrystal.h>
+
+#define LM35_PIN A0
+
+LiquidCrystal lcd (12, 11, 5, 4, 3, 2);
+
+void setup() {
+    lcd.begin(16, 2);
+    lcd.print("TEMP: ");
+    lcd.setCursor(0, 1); 
+}
+
+float lerTemperatura() {
+    float valorADC = analogRead(LM35_PIN);
+    float tensao = (valorADC * 5) / 1023;
+    float temperatura = tensao / 0.01;
+    return temperatura;
+}
+
+void imprimirLcd(float val) {
+    lcd.print(temp);
+}
+
+void loop() {
+    float temperatura;
+    temperatura = lerTemperatura();
+    imprimirLcd(temperatura);
+    delay(1000);
+}
